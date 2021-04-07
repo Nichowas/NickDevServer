5bc01e1e6ea4f69aa3f7782667283d9573c77642
Nichowas <nichowaswins@gmail.com>
2021-04-07 16:05:10 +1000

    commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const
     http = require("http").createServer(app),
     io = require("socket.io")(http)
 http.listen(port);
-
+console.log(port)
 io.on("connection", (client) => {
     client.on('msg', (txt) => {
         console.log(txt)
