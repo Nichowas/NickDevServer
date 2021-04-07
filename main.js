1b19b83b5b63530fee240a7c38afec7f96c263b8
Nichowas <nichowaswins@gmail.com>
2021-04-07 16:20:31 +1000

    commit

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const
     port = process.env.PORT || 4001,
     app = require("express")(),
     http = require("http").createServer(app),
-    io = require("socket.io")(http)
+    io = require("socket.io")(http, {
+        cors: {
+            origin: '*',
+        }
+    })
 http.listen(port);
 io.on("connection", (client) => {
     console.log(client)
