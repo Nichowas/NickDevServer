5329d9945a8615970fb5b87cbb87ca9197524668
Nichowas <nichowaswins@gmail.com>
2021-04-09 15:46:57 +1000

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const
-    port = process.env.PORT || 4001,
+    port = process.env.PORT,
     app = require("express")(),
     http = require("http").createServer(app),
     io = require("socket.io")(http, {
@@ -8,9 +8,22 @@ const
         }
     })
 http.listen(port);
+var socketCount = 0;
+var startTime = Date.now();
+var ls;
 io.on("connection", (client) => {
-    client.on('msg', (txt) => {
-        console.log(txt)
-        io.emit('msg', txt)
+    console.log(`${socketCount + 1} client${socketCount > 0 ? 's' : ''} connected`)
+    let room = Math.floor(socketCount / 2)
+    client.on('update', (angle) => {
+        client.to(`Game ${room}`).emit('update', { timestamp: Date.now() - startTime, data: angle })
     })
+
+    client.join(`Game ${room}`)
+    if (socketCount % 2 == 0) {
+        ls = client;
+    } else {
+        ls.emit('ready', 0)
+        client.emit('ready', 1)
+    }
+    socketCount++;
 })
